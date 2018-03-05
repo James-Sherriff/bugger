091dfa10ef85472bd84731de1773ac226cf01828
jshe10 <jamessherriff@warwickshire.gov.uk>
2018-03-05 12:25:38 +0000

    Adds test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "jamessherriff158@gmail.com", password: Devise.friendly_token(length = 20))
+User.create(email: "stuartwright@warwickshire.gov.uk", password: Devise.friendly_token(length = 20))
+User.create(email: "emiliorisi.gov.uk", password: Devise.friendly_token(length = 20))
+User.create(email: "connorbabbington@warwickshire.gov.uk", password: Devise.friendly_token(length = 20))
